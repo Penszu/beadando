26462255aaba2031a667829924b8c9aaf880f65b
Penszu <48126383+Penszu@users.noreply.github.com>
2019-05-11 13:36:32 +0200

    Add files via upload

--- 19.py
+++ 19.py
@@ -1,1 +1,59 @@
-print("github")
+ls = {'A': '.-', 'B': '-...',
+                       'C': '-.-.', 'D': '-..', 'E': '.',
+                       'F': '..-.', 'G': '--.', 'H': '....',
+                       'I': '..', 'J': '.---', 'K': '-.-',
+                       'L': '.-..', 'M': '--', 'N': '-.',
+                       'O': '---', 'P': '.--.', 'Q': '--.-',
+                       'R': '.-.', 'S': '...', 'T': '-',
+                       'U': '..-', 'V': '...-', 'W': '.--',
+                       'X': '-..-', 'Y': '-.--', 'Z': '--..',
+                       '1': '.----', '2': '..---', '3': '...--',
+                       '4': '....-', '5': '.....', '6': '-....',
+                       '7': '--...', '8': '---..', '9': '----.',
+                       '0': '-----', ', ': '--..--', '.': '.-.-.-',
+                       '?': '..--..', '/': '-..-.', '-': '-....-',
+                       '(': '-.--.', ')': '-.--.-'}
+
+
+def encrypt(message): #msg to morse
+    inmorse = ''
+    for i in message:
+        if i != ' ':
+            inmorse += ls[i] + ' '
+        else:
+            inmorse += '\t'
+
+    return inmorse
+
+def decrypt(message): #morse to msg
+    # message += ' '
+
+    regular = ''
+    citext = ''
+    for j in message.replace('\t', ' '): # j = letter
+        if (j != ' '):
+            space_in_between = 0
+            citext += j
+        else:
+            space_in_between += 1
+            if space_in_between == 2:
+                regular += ' '
+            else:
+                regular += list(ls.keys())[list(ls.values()).index(citext)]
+                citext = ''
+
+    return regular
+
+#MAIN
+
+message = input("Message: ") # <>={} []
+for i in message.upper():
+    if ("a" <= i <= "z") is True or ("A" <= i <= "Z") is True:
+        result_in_morse = encrypt(message.upper())
+        print("Original Message in Morse:", result_in_morse)
+        print("Original Message: ", message)
+        break
+    else:
+        result_in_regular = decrypt(message)
+        print("Original Message: ", message)
+        break
