266fd96c47a14d23cc8b10095f712d633a4e0add
Penszu <48126383+Penszu@users.noreply.github.com>
2019-05-15 18:25:40 +0200

    Add files via upload

--- 12.py
+++ 12.py
@@ -1,1 +1,17 @@
-print('aha')
+string = input("String: ") # <>={}
+num = 0
+kis = 0
+nagy = 0
+egyeb = 0
+kis_ls = ["á", "é", "í","ó", "ö", "ő", "ú", "ü", "ű"]
+nagy_ls = ["Á", "É", "Í", "Ó", "Ö", "Ő", "Ú", "Ü", "Ű"]
+for i in string.replace(" ", ""):
+    if "a" <= i <= "z" or i in kis_ls:
+        kis += 1
+    elif "A" <= i <= "Z" or i in nagy_ls:
+        nagy += 1
+    elif "0" <= i <= "9":
+        num += 1
+    else:
+        egyeb += 1
+print("num = {} kisbetűk = {} nagybetűk = {} egyéb = {}".format(num, kis, nagy, egyeb))
